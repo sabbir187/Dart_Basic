741335f9b6cbdc49b99dbd58c5d3989ce9185661
Md Sabbir Ahmed <77279024+sabbir187@users.noreply.github.com>
2023-12-30 20:51:01 +0600

    Add files via upload

---
+++ dart_basic/bin/dart_basic.dart
@@ -0,0 +1,15 @@
+import 'dart:ffi';
+
+import 'package:dart_basic/dart_basic.dart' ;
+
+void main(List<String> argument){
+  int age=16;
+  print(age.isOdd);
+  print(age.isEven);
+  print(age.bitLength);
+  String first='sabbir';
+  String last='Ahmed';
+  //concetanation
+  print('full name:$first $last');
+
+}
